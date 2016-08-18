a596fba7bec01727793776b45d2b32816fe0feaf
erhan-kartaltepe <erhan@thinkerhan.com>
2016-08-18 12:57:43 -0700

    Commiting directory--json file _+ driver change.

--- driver.py
+++ driver.py
@@ -4,3 +4,10 @@ class Driver:
 
 if __name__ == '__main__':
     Driver().main()
+
+# ID--name, city state, phone, kvps of headings based on type
+# Summary--sentences.
+# Experience--Company, city, state, from, to, title, list of stuff (sentence and type and sublist of skills)
+# Technical Skills--list (name and type)
+# Education--name, city, state, gpa, degree (BS, MS, Certification etc.), concentration
+# Awards--sentence
